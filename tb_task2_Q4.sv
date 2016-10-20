29795adc419a892e8b4e76999bd79388a16a51c9
HiralBarot <barothiral0310@gmail.com>
2016-10-20 02:57:30 -0700

    Update tb_task2_Q4.sv

--- tb_task2_Q4.sv
+++ tb_task2_Q4.sv
@@ -2,10 +2,10 @@
 /////////////////////////////////////////////////////////////////////////////////
 // Create Date: 10/15/2016 05:30:24 PM
 // Design Name: Hiral Barot
-// Module Name: tb_task_1
+// Module Name: Comparator
 // Project Name: Project 1 - Task1 testbench Task1
 //////////////////////////////////////////////////////////////////////////////////
-module tb_task_1;
+module Comparator;
 
 logic [2:0] x,y;
 logic eqo;
